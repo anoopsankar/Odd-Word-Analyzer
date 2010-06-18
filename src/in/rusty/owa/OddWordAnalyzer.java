0cd019c900c62de7e2959873b5892b0afb99e0b5
Anoop <anoop@mobme.in>
2010-06-18 12:40:47 +0530

    Completed everything.!
    Oddest word is SUCCINYLSULFATHIAZOLE.

--- src/in/rusty/owa/OddWordAnalyzer.java
+++ src/in/rusty/owa/OddWordAnalyzer.java
@@ -8,40 +8,81 @@ import java.net.URL;
 import java.util.HashMap;
 
 public class OddWordAnalyzer {
-	
-	public static int letterDistances[][] = new int [26][26];
-	
+
+	public static int letterDistances[][] = new int[26][26];
+
 	/**
 	 * @param args
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public static void main(String[] args) throws IOException {
-		
+
 		buildLetterDistances();
-		printLetterDistances();
+		// printLetterDistances();
+		findOddestWord();
+	}
+
+	private static void findOddestWord() throws IOException {
+		BufferedReader wordReader = new BufferedReader(new FileReader(
+				"/home/anoop/code/hobby/odd-word-analyzer/src/words.lst"));
+
+		String word = "";
+		String oddestWord = "";
+		int lowestWeight = Integer.MAX_VALUE;
+
+		while ((word = wordReader.readLine()) != null) {
+			word = word.toUpperCase();
+			int currentWeight = 0;
+			if (word.length() > 20 && !word.contains(".") && !word.contains("-") && !word.contains(" ")) {
+				for (int i = 0; i < word.length() - 1; i++) {
+					char thisLetter = word.charAt(i);
+					char nextLetter = word.charAt(i + 1);
+					if (thisLetter > 64 && nextLetter > 64 && thisLetter < 92
+							&& nextLetter < 92) {
+						currentWeight += letterDistances[thisLetter - 65][nextLetter - 65];
+					}
+				}
+				// System.out.println(currentWeight);
+
+				currentWeight = currentWeight / word.length();
+
+				if (currentWeight < lowestWeight) {
+					lowestWeight = currentWeight;
+					oddestWord = word;
+				}
+			}
+		}
+		wordReader.close();
+
+		System.out
+				.println("The word with the oddest spelling in the English language is "
+						+ oddestWord);
 	}
 
 	private static void buildLetterDistances() throws IOException {
-		//URL wordList = OddWordAnalyzer.class.getResource("words.lst");
-		BufferedReader wordReader = new BufferedReader(new FileReader("/home/anoop/code/hobby/odd-word-analyzer/src/words.lst"));
-		
+		BufferedReader wordReader = new BufferedReader(new FileReader(
+				"/home/anoop/code/hobby/odd-word-analyzer/src/words.lst"));
+
 		String word = "";
 		while ((word = wordReader.readLine()) != null) {
-			analyzeWord(word.toUpperCase());
+			updateFrequencyFromWord(word.toUpperCase());
 		}
+		wordReader.close();
 	}
 
-	private static void analyzeWord(String word) {
+	private static void updateFrequencyFromWord(String word) {
 		for (int i = 0; i < word.length() - 1; i++) {
-			
+
 			char thisLetter = word.charAt(i);
 			char nextLetter = word.charAt(i + 1);
-			if (thisLetter > 64 && nextLetter > 64 && thisLetter < 92 && nextLetter < 92) {
+			if (thisLetter > 64 && nextLetter > 64 && thisLetter < 92
+					&& nextLetter < 92) {
 				letterDistances[thisLetter - 65][nextLetter - 65]++;
 			}
 		}
 	}
-	
+
+	// Debug
 	private static void printLetterDistances() {
 		for (int i = 65; i <= 65 + 26; i++) {
 			System.out.print("\t" + (char) i);
@@ -54,7 +95,7 @@ public class OddWordAnalyzer {
 			}
 			System.out.println("");
 		}
-		
+
 	}
 
 }
